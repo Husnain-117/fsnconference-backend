5dd24b089ee0f19ad3e5e8782f1088ed027f6321
Husnain Akram <husnainakram525@gmail.com>
2025-08-05 20:10:40 +0500

    updated

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import registrationRoutes from './routes/registrationRoutes.js';
+import contactRoutes from './routes/contactRoutes.js';
+
 dotenv.config();
 
 const app = express();
@@ -12,6 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', registrationRoutes);
+app.use('/api', contactRoutes);
 
 app.get('/', (req, res) => {
   res.send('Backend for Conference Registration is running.');
